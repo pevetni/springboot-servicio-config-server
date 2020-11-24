afd415fcb480e2cafaa091152e8f37b32c9f0e17
carlos.rodriguez <carlos.rodriguez@appdirect.com>
2020-11-24 11:43:27 -0300

    upload version of java and version of spring cloud, point to the config repo on github

--- src/test/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplicationTests.java
+++ src/test/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplicationTests.java
@@ -1,13 +1,16 @@
 package com.formacionbdi.springboot.app.config;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootServicioConfigServerApplicationTests {
+public class SpringbootServicioConfigServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
